19ed5cb68bca8a42bc4edae6b4c79409dc2cf4ad
CEDRIC LV <[cedriclevalegant@gmail.com](mailto:cedriclevalegant@gmail.com)>
2023-03-07 16:08:34 +0100

    first commit

--- src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/WizardController.java
@@ -1,7 +1,10 @@
 package com.wildcodeschool.wildandwizard.controller;
 
 import com.wildcodeschool.wildandwizard.entity.Wizard;
-import com.wildcodeschool.wildandwizard.repository.WizardRepository;
+import com.wildcodeschool.wildandwizard.repository.WizardDao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,23 +15,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class WizardController {
 
-    private WizardRepository repository = new WizardRepository();
+    @Autowired
+    @Qualifier("wizardRepository")
+    private WizardDao wizardDao;
 
     @GetMapping("/wizards")
     public String getAll(Model model) {
 
-        model.addAttribute("wizards", repository.findAll());
+        model.addAttribute("wizards", wizardDao.findAll());
 
         return "wizards";
     }
 
     @GetMapping("/wizard")
-    public String getWizard(Model model,
-                            @RequestParam(required = false) Long id) {
-
+    public String getWizard(Model model, @RequestParam(required = false) Long id) {
         Wizard wizard = new Wizard();
         if (id != null) {
-            wizard = repository.findById(id);
+            wizard = wizardDao.findById(id);
         }
         model.addAttribute("wizard", wizard);
 
@@ -39,9 +42,9 @@ public class WizardController {
     public String postWizard(@ModelAttribute Wizard wizard) {
 
         if (wizard.getId() != null) {
-            repository.update(wizard);
+            wizardDao.update(wizard);
         } else {
-            repository.save(wizard);
+            wizardDao.save(wizard);
         }
         return "redirect:/wizards";
     }
@@ -49,7 +52,7 @@ public class WizardController {
     @GetMapping("/wizard/delete")
     public String deleteWizard(@RequestParam Long id) {
 
-        repository.deleteById(id);
+        wizardDao.deleteById(id);
 
         return "redirect:/wizards";
     }
